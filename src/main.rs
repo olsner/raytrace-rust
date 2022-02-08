36242746779847c733bf76eb94955007d6fe9b30
Simon Brenner <olsner@gmail.com>
2022-02-08 23:01:43 +0100

    Multiple spheres/shapes and use closest hit

--- src/main.rs
+++ src/main.rs
@@ -46,34 +46,84 @@ impl Sphere {
     }
 }
 
+struct HitRecord {
+    point : Point3<f32>,
+    normal : UVec3,
+    distance : f32,
+}
+
 trait Shape {
-    fn hit(self : &Self, ray : &Ray) -> f32;
+    fn hit(self : &Self, ray : &Ray) -> Option<HitRecord>;
 }
 
 impl Shape for Sphere {
-    fn hit(self : &Sphere, r : &Ray) -> f32 {
+    fn hit(self : &Sphere, r : &Ray) -> Option<HitRecord> {
         let oc = r.origin - self.center;
         let a = r.direction.norm_squared();
         let hb = oc.dot(&r.direction);
         let c = oc.norm_squared() - self.radius * self.radius;
         let discriminant = hb * hb - a * c;
         if discriminant < 0.0 {
-            -1.0
-        } else {
-            (-hb - discriminant.sqrt()) / a
+            return None;
+        }
+
+        let distance = (-hb - discriminant.sqrt()) / a;
+        let point = r.at(distance);
+        let normal = UVec3::new_normalize(point - self.center);
+        if distance >= 0.0 {
+            Some(HitRecord{ point, normal, distance })
+        }
+        else {
+            None
         }
     }
 }
 
-fn ray_color(ray : &Ray) -> RGBf32 {
+fn best(left : Option<HitRecord>, right : Option<HitRecord>) -> Option<HitRecord> {
+    match left {
+        None => right,
+        Some(lhit) => Some(match right {
+            Some(rhit) =>
+                if lhit.distance < rhit.distance { lhit } else { rhit }
+            None => lhit,
+        })
+    }
+}
+
+struct Scene {
+    spheres : Vec<Sphere>,
+}
+
+impl Scene {
+    fn new() -> Self {
+        Scene { spheres : Vec::new() }
+    }
+
+    fn add(self : &mut Self, sphere : Sphere) {
+        self.spheres.push(sphere);
+    }
+}
+
+impl Shape for Scene {
+    fn hit(self : &Scene, r : &Ray) -> Option<HitRecord> {
+        let mut best_hit = None;
+        for sphere in &self.spheres {
+            best_hit = best(sphere.hit(r), best_hit);
+        }
+        return best_hit;
+    }
+}
+
+fn ray_color(world : &impl Shape, ray : &Ray) -> RGBf32 {
     let white = Vec3::new(1.0, 1.0, 1.0);
     let blue = Vec3::new(0.5, 0.7, 1.0);
 
-    let sphere = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5);
-    let dist = sphere.hit(ray);
-    if dist >= 0. {
-        let n = (ray.at(dist) - sphere.center).normalize();
-        return RGBf32::from(0.5 * (n + Vec3::repeat(1.0)));
+    match world.hit(ray) {
+        Some(hit) => {
+            let n = hit.normal.into_inner();
+            return RGBf32::from(0.5 * (n + Vec3::repeat(1.0)));
+        }
+        None => ()
     }
 
     let unit_direction = ray.direction;
@@ -83,6 +133,7 @@ fn ray_color(ray : &Ray) -> RGBf32 {
 
 fn write_ppm<Pixel>(path : &Path, buf : &Framebuf<Pixel>)
   where RGBu8 : From<Pixel>, Pixel : Copy {
+    print!("Writing {}\n", path.display());
     let mut file = File::create(&path).unwrap();
     write!(&mut file, "P3\n{} {}\n255\n", buf.width, buf.height).unwrap();
     for y in (0..buf.height).rev() {
@@ -112,6 +163,11 @@ fn main() {
     let lower_left_corner = origin - horizontal / 2. - vertical / 2. -
         Vec3::new(0., 0., focal_length);
 
+    let mut world = Scene::new();
+    world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
+    world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.));
+
+    // Render
     for y in (0..height).rev() {
         for x in 0..width {
             let u = x as f32 / (width - 1) as f32;
@@ -119,7 +175,7 @@ fn main() {
 
             let direction = UVec3::new_normalize(lower_left_corner + u*horizontal + v*vertical - origin);
             let r = Ray { origin, direction };
-            fbuf[(x, y)] = ray_color(&r);
+            fbuf[(x, y)] = ray_color(&world, &r);
         }
     }
 
